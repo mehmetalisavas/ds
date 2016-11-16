d66835f4d024dd3300eaa127086af27132cb3623
mehmetalisavas <mehmetali.savas@yandex.com>
2016-11-16 18:05:01 +0300

    tests/binary_tree: root & isLeaf tests are added

--- tree/binary_tree/binary_tree_test.go
+++ tree/binary_tree/binary_tree_test.go
@@ -34,10 +34,7 @@ func TestBinaryTreeRootSet(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	setRoot, err := tree.SetRoot("set-root")
-	if err != nil {
-		t.Fatal(err)
-	}
+	setRoot := tree.SetRoot("set-root")
 	if tree.Root() != setRoot {
 		t.Fatal("tree root should equal set-root")
 	}
@@ -124,5 +121,81 @@ func TestBinaryAddTwoTimesRight(t *testing.T) {
 	}
 	if root.Right().Right() != right2 {
 		t.Fatal("right child of root should equal to right2 node")
+	}
+}
+func TestBinaryTreeParent(t *testing.T) {
+	tree := NewTree()
+	root, err := tree.AddRoot("root")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if root.Parent() != root {
+		t.Fatal("parent of root should equal ")
+	}
+}
+
+func TestBinaryTreeParentNodes(t *testing.T) {
+	tree := NewTree()
+	root, err := tree.AddRoot("root")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	left := root.AddLeft("root-left")
+	left2 := left.AddLeft("root-left2")
+
+	if left.Parent() != root {
+		t.Fatal("parent of left should equal root")
+	}
+	if left2.Parent() != left {
+		t.Fatal("parent of left should equal root")
+	}
+}
+
+func TestBinaryTreeRootOfNodes(t *testing.T) {
+	tree := NewTree()
+	root, err := tree.AddRoot("root")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	left := root.AddLeft("root-left")
+	left2 := left.AddLeft("root-left2")
+	left3 := left.AddLeft("root-left3")
+
+	if left2.Root() != root {
+		t.Fatal("root of left2 should equal root")
+	}
+
+	if left3.Root() != root {
+		t.Fatal("root of left3 should equal root")
+	}
+}
+
+func TestBinaryTreeLeaves(t *testing.T) {
+	tree := NewTree()
+	root, err := tree.AddRoot("root")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	left := root.AddLeft("root-left")
+	left2 := left.AddLeft("root-left2")
+	right := root.AddRight("root-right")
+
+	if root.IsLeaf() == true {
+		t.Fatal("isLeaf should equal false")
+	}
+	if left.IsLeaf() == true {
+		t.Fatal("isLeaf should equal false")
+	}
+
+	if left2.IsLeaf() != true {
+		t.Fatal("isLeaf should equal true")
+	}
+
+	if right.IsLeaf() != true {
+		t.Fatal("isLeaf should equal true")
 	}
 }
