6821125ec25cc676180633783d8cfb4d5fdfc7be
mehmetalisavas <mehmetali.savas@yandex.com>
2016-12-09 01:39:38 +0300

    bst: add comment for Search function of binary search tree

--- tree/binary_tree/binary_search_tree.go
+++ tree/binary_tree/binary_search_tree.go
@@ -1,6 +1,8 @@
 package tree
 
 // Search searches the given integer value inside the binary tree
+// If given value is greater than root's value, then search right nodes of tree
+// otherwise search in the left nodes of the root node
 func Search(root *Node, value int) *Node {
 	if root == nil || root.Value().(int) == value {
 		return root
